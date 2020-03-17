fc824431e538d1b4f884ba1b60477935ae6279ee
Vishnu V P (vivp) <vivp@cisco.com>
2020-03-17 19:37:13 +0530

    Update ghpages script

--- ghpush_script.sh
+++ ghpush_script.sh
@@ -1,2 +1,9 @@
 python3 py/collect_clean_data.py
 
+git add . -A 
+git commit -m "Update data"
+git checkout gh-pages
+git checkout master covid19-data.csv
+git add . -A
+git commit -m "Update data"
+git push origin gh-pages
