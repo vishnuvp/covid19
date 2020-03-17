364c4ba1320434a96403be21f37e35d8007d05e6
Vishnu V P (vivp) <vivp@cisco.com>
2020-03-18 00:09:37 +0530

    Update data

---
+++ ghpush_script.sh
@@ -0,0 +1,10 @@
+python3 py/collect_clean_data.py
+[[ ! -z "$1" ]] && echo $1 >> covid19-data.csv
+git add . -A 
+git commit -m "Update data"
+git checkout gh-pages
+git checkout master covid19-data.csv
+git add . -A
+git commit -m "Update data"
+git push origin gh-pages
+git checkout master
