f72be978a17bb268fbd248fd4226bda9168541ca
Vishnu V P (vivp) <vivp@cisco.com>
2020-03-21 17:51:43 +0530

    Update data

--- ghpush_script.sh
+++ ghpush_script.sh
@@ -1,12 +1,57 @@
-python3 py/collect_clean_data.py
-[[ ! -z "$1" ]] && echo $1,,,,,,,,,,,,,,,, >> covid19-data.csv
-python3 py/collect_state_data.py
-git add . -A 
-git commit -m "Update data"
-git checkout gh-pages
-git checkout housekeeping covid19-data.csv
-git checkout housekeeping covid19-india.json
-git add . -A
-git commit -m "Update data"
+for arg in "$@"
+do
+    case $arg in
+        -w|--world)
+		git checkout housekeeping
+        python3 py/collect_clean_data.py
+        git add . -A 
+		git commit -m "Update data"
+		git checkout gh-pages
+		git checkout housekeeping covid19-data.csv
+		git add . -A
+		git commit -m "Update data"
+        shift 
+        ;;
+        -i|--india)
+		git checkout housekeeping
+        python3 py/collect_state_data.py
+        git add . -A 
+		git commit -m "Update data"
+		git checkout gh-pages
+		git checkout housekeeping covid19-india.json
+		git add . -A
+		git commit -m "Update data"
+        shift 
+        ;;
+        -g|--git-alone)
+		git checkout housekeeping
+		git add . -A 
+		git commit -m "Update data"
+		git checkout gh-pages
+		git checkout housekeeping covid19-india.json
+		git checkout housekeeping covid19-data.csv
+		git add . -A
+		git commit -m "Update data"
+		;;
+        -a|--all)
+        python3 py/collect_clean_data.py
+        python3 py/collect_state_data.py
+
+        shift 
+        ;;
+        -u|--update-world)
+		echo $2
+        # echo $2,,,,,,,,,,,,,,,, >> covid19-data.csv
+
+        shift # Remove argument name from processing
+        shift # Remove argument value from processing
+        ;;
+        *)
+        OTHER_ARGUMENTS+=("$1")
+        shift # Remove generic argument from processing
+        ;;
+    esac
+done
+
 git push origin gh-pages
 git checkout housekeeping
