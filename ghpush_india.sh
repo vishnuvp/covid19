b0db67c0c0036642ca96430d6ff84f914696791b
Vishnu V P (vivp) <vivp@cisco.com>
2020-03-29 15:59:31 +0530

    Add India status doughnut and refactor

--- ghpush_india.sh
+++
@@ -1,9 +0,0 @@
-python3 py/collect_state_data.py
-git add . -A 
-git commit -m "Update data"
-git checkout gh-pages
-git checkout housekeeping covid19-india.json
-git add . -A
-git commit -m "Update data"
-git push origin gh-pages
-git checkout housekeeping
